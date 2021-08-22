80a52667709ea5a06321a7ecde20ee1058261691
Mia Thomas <miathomas1102@gmail.com>
2021-08-22 20:54:55 +0100

    Tidied up my code

--- Affirmations/script.sh
+++ Affirmations/script.sh
@@ -1,12 +1,6 @@
 # Affirmations Task - Objective:
-
-
 echo "Hello, how many Affirmations would you like to receive today?"
-
 read affirmationRequest
-
-echo "= $affirmationRequest affirmations are now available to read in the Affirmations file." 
-
 i=0
 
 while [ $i -lt $affirmationRequest ]
@@ -14,8 +8,7 @@ do
     websiteOutput=$(curl "https://www.affirmations.dev/")
     websiteOutput=$(echo "${websiteOutput}" | jq '.affirmation' >> Affirmations.txt)
     i=$[$i+1]
- done
-
+done
 
 numberOfAffirmations=$(wc -l Affirmations.txt)
 echo "Thanks for using our service, to generate your Affirmations, we've put $numberOfAffirmations in the file Affirmations.txt"
