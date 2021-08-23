f0fa8c61bb3089395766fc1c7a53343a5043b982
Mia Thomas <miathomas1102@gmail.com>
2021-08-23 09:48:41 +0100

    Removed excess spacing and added the wc -l command

--- Affirmations/script.sh
+++ Affirmations/script.sh
@@ -1,19 +1,18 @@
-# Affirmations Task - Objective:
-
+#!/bin/bash
 echo "Hello, how many Affirmations would you like to receive today?"
 read affirmationRequest
 i=0
+IFS=""
 declare -a affirmations=()
-
 while [ $i -lt $affirmationRequest ]
 do 
      websiteOutput=$(curl -s "https://www.affirmations.dev/")
      affirmations+=($(echo "${websiteOutput}" | jq '.affirmation'))
-    i=$[$i+1]
+     i=$[$i+1]
 done
-
 printf "%s\n" "${affirmations[@]}" > Affirmations.txt 
-
+numberOfAffirmations=$(cat Affirmations.txt | wc -l)
+echo "Thanks for using our service, to generate your Affirmations, we've put $numberOfAffirmations in the file Affirmations.txt"
 
 
 
